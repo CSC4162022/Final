b0ddca30ecb3f8dcd41019469213c03ff839b547
CD <98351932+CSC4162022@users.noreply.github.com>
2022-05-11 09:22:22 -0400

    Add files via upload

---
+++ NKU-Final-main/Characters/CharacterList.swift
@@ -0,0 +1,43 @@
+import SwiftUI
+
+struct CharacterList: View {
+    
+    @State var characters = [MarvelCharacter]()
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(characters) { MarvelCharacter in
+                        CharacterRow(character: MarvelCharacter)
+                                }
+                }
+                .onAppear{
+                    self.getCharacters()
+                }
+            }
+            .navigationTitle("Characters")
+        }.navigationViewStyle(StackNavigationViewStyle())
+    }
+    
+    func getCharacters() {
+        let networkManager = NetworkManager()
+        
+        networkManager.getCharacters(offset: 0, limit: 100) { container, error in
+            
+            guard let container = container, let characters = container.results, error == nil else {
+                return
+            }
+            
+            DispatchQueue.main.async {
+                self.characters = characters
+            }
+        }
+    }
+}
+
+struct CharacterList_Previews: PreviewProvider {
+    static var previews: some View {
+        CharacterList(characters: MarvelCharacter.characters)
+    }
+}
